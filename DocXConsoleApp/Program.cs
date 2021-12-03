b43c1c6f6f401ec9b28a4d8a0e9690fa34f44113
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2021-12-03 21:27:03 +0600

    Добавьте файлы проекта.

---
+++ DocXConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DocXConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
