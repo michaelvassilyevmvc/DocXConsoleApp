8f5820de42e781e6af024b658851f2feeb814061
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2021-12-03 22:38:41 +0600

    Настроено получение данных из одной таблицы

---
+++ DocXConsoleApp/Models/Competition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DocXConsoleApp.Models
+{
+    public class Competition
+    {
+        public string Name { get; set; }
+        public string DateAndPlace { get; set; }
+        public string Organization { get; set; }
+        public string Count { get; set; }
+        public string AthleteCount { get; set; }
+        public string TrainerCount { get; set; }
+        public string JudgesCount { get; set; }
+        public string OrganizerFirm { get; set; }
+        public string SendFirm { get; set; }
+    }
+}
--- DocXConsoleApp/Program.cs
+++ DocXConsoleApp/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+using System.Collections.Generic;
+using System.Linq;
+using DocXConsoleApp.Models;
 
 namespace DocXConsoleApp
 {
     class Program
     {
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<Competition> result = new List<Competition>();
+            using (var doc = WordprocessingDocument.Open(@"C:\Users\FAMILY\Documents\Результ.docx", false))
+            {
+                
+                Table table = doc.MainDocumentPart.Document.Body.Elements<Table>().First();
+
+                IEnumerable<TableRow> rows = table.Elements<TableRow>();
+
+
+                foreach (TableRow row in rows)
+                {
+                    List<TableCell> cells = row.Descendants<TableCell>().ToList();
+                    Competition competition = new Competition
+                    {
+                        Name =          cells[0].InnerText,
+                        DateAndPlace =  cells[1].InnerText,
+                        Organization =  cells[2].InnerText,
+                        Count =         cells[3].InnerText,
+                        AthleteCount =  cells[4].InnerText,
+                        TrainerCount =  cells[5].InnerText,
+                        JudgesCount =   cells[6].InnerText,
+                        OrganizerFirm = cells[7].InnerText,
+                        SendFirm =      cells[8].InnerText
+                    };
+
+                    result.Add(competition);
+                }
+
+                var tmp = result;
+
+                
+            }
         }
     }
 }
